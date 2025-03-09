e13263b6e22e991afbd211709a1f2acc94740730
SÉRGIO HENRIQUE OLIVEIRA VIEIRA <al81614@alunos.utad.pt>
2025-03-09 19:27:05 +0000

    utf8

--- classe1.py
+++ classe1.py
@@ -1,7 +1,54 @@
-def somar(a, b):
-    return a + b  # O return deve estar dentro da funÁ„o
 
-print(somar(1, 2))  # O print est· no nÌvel correto
+# # # try:
+            
+
+# # #     numero1=int(input("Digite o primeiro numero: "))
+
+# # #     numero2=int(input("Digite o segundo numero: "))
+
+# # #     soma=numero1+numero2
+
+# # #     print("A soma dos numeros resulta em: ",soma)
+
+# # # except ValueError:
+
+# # #     print("Digite apenas numeros interitos")
 
 
+#     if numero1%2==0:
+#         print("O numero sera par")
+#     else:
+#         print("O numero sera impar")
 
+# except ValueError:
+#     print("Digite apenas numeros interitos")
+
+
+# try:
+#     numero = int(input("Digite um numero para ver sua tabuada: "))
+
+#     print(f"Tabuada de {numero}:")
+    
+#     # Loop para imprimir a tabuada de 1 a 10
+#     for i in range(1, 11):
+#         resultado = numero * i
+#         print(f"{numero} x {i} = {resultado}")
+
+# except ValueError:
+#     print("Por favor, digite um numero valido.")
+ # -*- coding: utf-8 -*-
+
+try:
+    numero = int(input("Digite um n√∫mero para ver sua tabuada: "))
+
+    print(f"Tabuada de {numero}:")
+
+    # Loop para imprimir a tabuada de 1 a 10
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
+
+    print("Tabuada conclu√≠da com sucesso!")
+
+except ValueError:
+    print("Por favor, digite um n√∫mero v√°lido.")
