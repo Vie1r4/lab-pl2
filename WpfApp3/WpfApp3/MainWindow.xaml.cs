d624d41c042488eec3487615339f52a4e6a70c9e
SÉRGIO HENRIQUE OLIVEIRA VIEIRA <al81614@alunos.utad.pt>
2025-03-10 10:12:12 +0000

    calculadora

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculadora
+{
+    public partial class MainWindow : Window
+    {
+        private string input = "";
+        private double num1 = 0, num2 = 0;
+        private char operation;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string content = button.Content.ToString();
+
+            if (double.TryParse(content, out _))
+            {
+                input += content;
+                txtResultado.Text = input;
+            }
+            else if (content == "C")
+            {
+                input = "";
+                num1 = num2 = 0;
+                txtResultado.Text = "";
+            }
+            else if (content == "=")
+            {
+                num2 = double.Parse(input);
+                double result = 0;
+
+                switch (operation)
+                {
+                    case '+': result = num1 + num2; break;
+                    case '-': result = num1 - num2; break;
+                    case '*': result = num1 * num2; break;
+                    case '/': result = num2 != 0 ? num1 / num2 : 0; break;
+                }
+
+                txtResultado.Text = result.ToString();
+                input = result.ToString();
+            }
+            else
+            {
+                num1 = double.Parse(input);
+                operation = content[0];
+                input = "";
+            }
+        }
+    }
+}
---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+
+namespace Calculadora
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double valor1 = Convert.ToDouble(txtValor1.Text);
+                double valor2 = Convert.ToDouble(txtValor2.Text);
+                string operacao = txtOperacao.Text;
+                double resultado = 0;
+
+                switch (operacao)
+                {
+                    case "+":
+                        resultado = valor1 + valor2;
+                        break;
+                    case "-":
+                        resultado = valor1 - valor2;
+                        break;
+                    case "*":
+                        resultado = valor1 * valor2;
+                        break;
+                    case "/":
+                        if (valor2 == 0)
+                        {
+                            txtResultado.Text = "Divisão por zero";
+                            return;
+                        }
+                        resultado = valor1 / valor2;
+                        break;
+                    default:
+                        txtResultado.Text = "Operador errado";
+                        return;
+                }
+
+                txtResultado.Text = resultado.ToString();
+            }
+            catch (Exception)
+            {
+                txtResultado.Text = "Erro!";
+            }
+        }
+    }
+}
